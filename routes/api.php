eaa43d805cbf289396cac7155415b7b9425fdd2a
DarioGiusino <dario.giusino@gmail.com>
2023-03-15 19:41:58 +0100

    defined api route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\api\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//project api routes (ALWAYS api/ at the start of any api routes)
+Route::get('/projects', [ProjectController::class, 'index']);

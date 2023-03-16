c1e781901a0fb0f69cc4f650ffaeeb6a91da136f
DarioGiusino <dario.giusino@gmail.com>
2023-03-16 16:55:00 +0100

    defined api route for single project

--- app/Http/Controllers/api/ProjectController.php
+++ app/Http/Controllers/api/ProjectController.php
@@ -31,7 +31,8 @@ class ProjectController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $project = Project::where('id', $id)->get();
+        return response()->json($project);
     }
 
     /**
